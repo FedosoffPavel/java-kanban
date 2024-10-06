87d99522632fe375db34b079451d84b4f063094b
Fedosov Pavel <i@pavelfedosoff.ru>
2024-10-06 20:13:56 +0300

    Удалил метод updateStatusEpic в классе Epic.

--- src/Epic.java
+++ src/Epic.java
@@ -17,29 +17,6 @@ public class Epic extends Task {
         return subtasks;
     }
 
-    public void updateStatusEpic() {
-        if (subtasks.isEmpty()) {
-            this.status = Status.NEW;
-        } else {
-            boolean allDone = true;
-            boolean anyInProgress = false;
-            for (Subtask subtask : subtasks) {
-                if (subtask.getStatus() == Status.IN_PROGRESS) {
-                    anyInProgress = true;
-                }
-                if (subtask.getStatus() != Status.DONE) {
-                    allDone = false;
-                }
-            }
-            if (allDone) {
-                this.status = Status.DONE;
-            } else if (anyInProgress) {
-                this.status = Status.IN_PROGRESS;
-            } else {
-                this.status = Status.NEW;
-            }
-        }
-    }
     @Override
     public String toString() {
         return "Epic{" +
