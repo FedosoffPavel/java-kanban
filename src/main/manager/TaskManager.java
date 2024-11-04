17aea9e499e19c06b958e193b6728550a42a3dfd
Fedosov Pavel <i@pavelfedosoff.ru>
2024-11-04 12:12:42 +0300

    Исправил ошибки в сcode stile в interface TaskManager

--- src/main/manager/TaskManager.java
+++ src/main/manager/TaskManager.java
@@ -8,29 +8,42 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<Subtask> getAllSubtasks();
 
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
 
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
 
     void createTask(Task task);
+
     void createEpic(Epic epic);
+
     void createSubtask(Subtask subtask);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
 
     List<Subtask> getSubtasksOfEpic(int epicId);
+
     List<Task> getHistory();
 }
